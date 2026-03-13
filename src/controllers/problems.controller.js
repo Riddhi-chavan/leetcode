9056290e5fd99159c9f95779ab95fa5ed6c4d368
Riddhi Chavan <riddhic164@gmail.com>
2026-03-13 20:47:15 +0530

    added get-problem and get-problem by id api

--- src/controllers/problems.controller.js
+++ src/controllers/problems.controller.js
@@ -84,3 +84,41 @@ export const createProblem = async (req, res) => {
         res.status(500).json({ error: "Failed to create problem" })
     }
 }
+
+export const getAllProblems = async (req, res) => {
+    try {
+        const problems = await prisma.problem.findMany()
+
+        res.status(200).json({
+            success: true,
+            problems
+        })
+
+    } catch (error) {
+        console.error("Error fetching problems", error.message)
+        res.status(500).json({ error: "Failed to fetch problems" })
+    }
+}
+
+export const getProblem = async (req, res) => {
+    const { id } = req.params
+
+    try {
+        const problem = await prisma.problem.findUnique({
+            where: { id }
+        })
+
+        if (!problem) {
+            return res.status(404).json({ error: "Problem not found" })
+        }
+
+        res.status(200).json({
+            success: true,
+            problem
+        })
+
+    } catch (error) {
+        console.error("Error fetching problem", error.message)
+        res.status(500).json({ error: "Failed to fetch problem" })
+    }
+}
--- src/routes/problems.routes.js
+++ src/routes/problems.routes.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import {authenticate , checkAdmin} from "../middleware/auth.middleware.js"
-import { createProblem } from "../controllers/problems.controller.js";
+import { createProblem, getAllProblems, getProblem } from "../controllers/problems.controller.js";
 
 const problemRoutes = Router();
 
 problemRoutes.post("/create-problem" , authenticate , checkAdmin , createProblem)
+problemRoutes.get("/get-all-problems", authenticate, getAllProblems)
+problemRoutes.get("/get-problem/:id", authenticate, getProblem)
 
 export default problemRoutes;
