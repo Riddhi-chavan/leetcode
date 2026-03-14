534ccec6c6725d9f7677fa5857cd8bc05d53b07b
Riddhi Chavan <riddhic164@gmail.com>
2026-03-15 02:52:46 +0530

    set up frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from './Components/Navbar'
+
+const App = () => {
+  return (
+    <div className='bg-[#eceff1] h-screen w-screen'>
+      <Navbar/>
+    </div>
+  )
+}
+
+export default App
