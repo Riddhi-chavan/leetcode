f0639f25a40e950443e55dbe96e4708d58837aba
Riddhi Chavan <riddhic164@gmail.com>
2026-03-19 01:53:56 +0530

    added basic design for login page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import Navbar from '../Components/Navbar'
+import leetcodeLoginLogo from "../assets/leetcodeLoginLogo.svg"
 
 const Login = () => {
   return (
-    <div className='bg-[#eceff1] h-screen w-screen'>
-      <Navbar/>
+     <div className='bg-[#eceff1] h-screen w-screen flex flex-col'>
+      <Navbar />
+      <div className='flex-1 flex items-center justify-center '>
+        <div className='w-[400px] bg-[#fff] h-[548px] p-[40px] flex flex-col items-center'>
+            <img src={leetcodeLoginLogo} alt="leetcodeLoginLogo" className='mb-[35px]' />
+            <div>
+          hiii
+        </div>  
+        </div>
+        
+      </div>
     </div>
   )
 }
